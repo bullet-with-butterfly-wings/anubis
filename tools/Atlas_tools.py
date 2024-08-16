0a2ca01d56f4598ef732e856547accf7f993df2d
bullet-with-butterfly-wings <dej.jonas12@gmail.com>
2024-08-16 11:24:34 +0100

    Abs times on triggers and bcr

--- tools/Atlas_tools.py
+++ tools/Atlas_tools.py
@@ -7,7 +7,10 @@ import os
 import matplotlib.colors as colors
 import matplotlib.backends.backend_pdf
 import hist as hi
+import datetime
 import importlib
+import uproot
+
 #sys.path.insert(1, 'C://Users//Peter//OneDrive - University of Cambridge//Desktop//summer2//Osiris Temp//processing//python')
 sys.path.append(os.path.join(sys.path[0], 'Osiris Temp', 'processing', 'python'))
 import rawFileReader
@@ -23,24 +26,28 @@ class BCR():
     def __init__(self, hitTime, event_time, bcr_count):
         self.hitTime = hitTime
         self.event_time = event_time
-        self.absTime = 0
-        self.bcr_count = bcr_count # negative because timestamp at the end
+        self.absTime = event_time+datetime.timedelta(seconds=89100/10**9)*bcr_count
+        self.bcr_count = bcr_count
         self.triggers = []
     
     def __str__(self):
-        return f"""BCR({self.hitTime}*25/32 ns, {self.absTime}, {self.bcr_count}) \n    Triggers: {[str(t) for t in self.triggers]})"""
+        return f"""BCR({self.hitTime}*25/32 ns, {self.absTime}, {self.event_time},{self.bcr_count}) \n    Triggers: {[str(t) for t in self.triggers]})"""
 
     def add_trigger(self, trigger):
-        #set_the absolute time for the trigger
+        trigger.absTime = self.absTime+(trigger.hitTime-self.hitTime)*datetime.timedelta(seconds=(25/32)/10**9)
+        trigger.bcId = (trigger.hitTime-self.hitTime)/32.
         self.triggers.append(trigger)
+
 
 class Trigger():
     def __init__(self, hitTime):
         self.hitTime = hitTime
+        self.bcId = 0
         self.absTime = 0
     
     def __str__(self):
         return f"Trigger({self.hitTime}*25/32 ns, {self.absTime})"
+
 
 class AtlasAnalyser():
     def __init__(self, file, trigger_channel, bcr_channel = 0):
@@ -49,17 +56,34 @@ class AtlasAnalyser():
         self.trigger_channel = trigger_channel
         self.bcr_channel = bcr_channel
         
-    def printEvents(self, amount_of_bcr):
+    def printBCR(self, amount_of_bcr):
         i = 0
         while i < amount_of_bcr:
             print(self.data[i])
             i += 1
 
-            
+    def printEvents(self, amount_of_events):
+        last_event_time = datetime.datetime(1960,2,3) 
+        evt_num = -1 #offset one
+        bcr_num = 0
+        while evt_num < amount_of_events: #i dont like the code :(
+            if self.data[bcr_num].event_time != last_event_time:
+                evt_num += 1
+                last_event_time = self.data[bcr_num].event_time
+            if evt_num < amount_of_events:
+                print(self.data[bcr_num])
+            if bcr_num+1 < len(self.data):
+                print("Check:", self.data[bcr_num+1].hitTime-self.data[bcr_num].hitTime)
+                bcr_num += 1
+            else:
+                evt_num = amount_of_events
+    
     def getTDC5Data(self, amount_of_events):
         i = 0
         initial_time = None
+        previous_event_time = None
         while i < amount_of_events:
+
             if not self.fReader.readBlock():
                 print("Finished Reading File")
                 return
@@ -69,11 +93,14 @@ class AtlasAnalyser():
                     continue
                 else:
                     i += 1
-                
-                bcr_count = 0
+
+                if i == 1:
+                    previous_event_time = tdc5Reads[0][0]
+                    continue
+
+                bcr_count = 1
                 tdcEvent = tdc5Reads[0] #actual pile of data (timestamp, data)
                 current_bcr = BCR(-1, tdcEvent[0], bcr_count)
-
                 if not initial_time:
                     initial_time = tdcEvent[0]
                     print("Initial Time:", initial_time)
@@ -83,7 +110,8 @@ class AtlasAnalyser():
                     if tdcChannel == self.bcr_channel:
                         if tdcHitTime != current_bcr.hitTime: #avoid repetition
                             self.data.append(current_bcr)
-                            current_bcr = BCR(tdcHitTime, tdcEvent[0], bcr_count)
+                            # looking at the event time before
+                            current_bcr = BCR(tdcHitTime, previous_event_time, bcr_count)
                             bcr_count += 1
                     elif tdcChannel == self.trigger_channel:
                         if not current_bcr:
@@ -93,9 +121,20 @@ class AtlasAnalyser():
                             current_bcr.triggers.append(Trigger(tdcHitTime))
                         else:
                             if tdcHitTime != current_bcr.triggers[-1].hitTime:
-                                current_bcr.triggers.append(Trigger(tdcHitTime))
+                                current_bcr.add_trigger(Trigger(tdcHitTime))
                     else:
                         pass
                         #print("Unknown Channel", tdcChannel)
+                
         self.data = self.data[1:] #remove the first empty BCR
         return self.data
+
+    def getAtlasData(self):
+        infile = '/eos/user/m/mireveri/anubis/athena/run/ATLASData/data-ANALYSIS/datafiles.root'
+        testFile = uproot.open(infile)
+        anaTree = testFile["analysis"]
+        feats = [branch.name for branch in anaTree.branches]
+        evtArr = anaTree.arrays(feats,library="pd")
+        evtArr
+        thisEvt = 9580
+        print(datetime.datetime.fromtimestamp(evtArr['TimeStamp'][thisEvt][0]),evtArr['TimeStampNS'][thisEvt][0],evtArr['BCID'][thisEvt][0])
