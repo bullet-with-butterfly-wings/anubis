97ef926764ac0ecc2940f1c63d886838605c0630
bullet-with-butterfly-wings <dej.jonas12@gmail.com>
2024-09-06 18:38:39 +0100

    background discovered

--- tools/Atlas_tools.py
+++ tools/Atlas_tools.py
@@ -19,6 +19,7 @@ sys.path.append(os.path.join(sys.path[0], 'Osiris Temp', 'processing', 'python')
 import rawFileReader
 import Reconstruction_tools as RTools
 import Timing_tools as TTools
+import gc
 importlib.reload(RTools)
 importlib.reload(TTools)
 
@@ -28,27 +29,27 @@ def readTimeStamp(timestamp):
 
 
 class BCR():
-    def __init__(self, hitTime, event_time, bcr_count, error = False):
+    def __init__(self, hitTime, event_time, error = False):
         self.hitTime = hitTime
         self.event_time = event_time
         self.timeStamp = datetime.datetime.timestamp(event_time) #+bcr_count*89100/10**9
-        self.bcr_count = bcr_count #Do I needed??
         self.triggers = []
         self.error = error
     
     def __str__(self):
-        return f"""BCR({self.hitTime}*25/32 ns, {readTimeStamp(self.timeStamp)}, {self.event_time},{self.bcr_count}, {self.error}) \n    Triggers: {[str(t) for t in self.triggers]})"""
+        return f"""BCR({self.hitTime}*25/32 ns, {readTimeStamp(self.timeStamp)}, {self.event_time}, {self.error}) \n    Triggers: {[str(t) for t in self.triggers]})"""
 
     def add_trigger(self, trigger):
         delta = (trigger.hitTime-self.hitTime)
         if delta < 0:
             delta += 1_048_575
-            
+
+        if delta/32 > 3564 and not self.error:
+            pass
         trigger.timeStamp = self.timeStamp+delta*(25/32)/10**9
         #print("Trigger", trigger.hitTime)
         #print("Bcr", self.hitTime)
         trigger.bcId = delta/32.
-        trigger.error = self.error
         """
         if trigger.bcId > 3564:
             print("Problem")
@@ -60,14 +61,12 @@ class BCR():
         """
         self.triggers.append(trigger)
 
-
 class Trigger():
     def __init__(self, hitTime):
         self.hitTime = hitTime
         self.bcId = 0
         self.timeStamp = 0
-        self.error = False
-    
+       
     def __str__(self):
         return f"Trigger({self.hitTime}*25/32 ns, {self.timeStamp}, {self.bcId})"
 
@@ -105,6 +104,7 @@ class AtlasAnalyser():
                 evt_num = amount_of_events
 
     def correctionBCR(self):
+        gc.disable()
         last = 0
         current = 0
         bad_stage = False
@@ -118,8 +118,9 @@ class AtlasAnalyser():
                     #print("Bad stage started:", current)
                     bad_stage = True
                     last = current - 1
-                    triggers = [] #handle triggers later
-                good_bcr = self.anubis_data[last]
+                    good_bcr = self.anubis_data[last]
+                    triggers = good_bcr.triggers.copy() #handle triggers later
+                    good_bcr.triggers = []
                 
                 if bad_stage:
                     #print("In bad stage:", current)
@@ -134,16 +135,21 @@ class AtlasAnalyser():
                         for bad_bcr in range(last+1, current): #remove and replace
                             del self.anubis_data[last+1]
                         #print(self.anubis_data[last-4:current+4])
-                        for new_bcr in range(1,prob_bcr_count):
-                            self.anubis_data.insert(last+new_bcr, BCR((good_bcr.hitTime+(new_bcr)*interval) % period, good_bcr.event_time, good_bcr.bcr_count)) # will need to think about other data
+                        for new_bcr in range(0,prob_bcr_count):
+                            if new_bcr != 0:
+                                self.anubis_data.insert(last+new_bcr, BCR((good_bcr.hitTime+(new_bcr)*interval) % period, good_bcr.event_time))
                             for trig in triggers:
                                 previous = (good_bcr.hitTime+(new_bcr)*interval) % period
                                 next = (good_bcr.hitTime+(new_bcr+1)*interval) % period
                                 overflow = False
                                 if previous > next:
                                     next += period
+                                    #print("Overflow")
                                     overflow = True
-                                if last < trig.hitTime < next or (overflow and previous < trig.hitTime + period < next):
+                                    #print("Last:", last)
+                                    #print(previous < trig.hitTime + period < next)
+                                    #print(last < trig.hitTime < next)
+                                if previous < trig.hitTime < next or (overflow and previous < trig.hitTime + period < next):
                                     self.anubis_data[last+new_bcr].add_trigger(trig)
                                     triggers.remove(trig)
                                 else:
@@ -178,7 +184,6 @@ class AtlasAnalyser():
                    
 
     def _readingRoutine(self, tdc5Reads,trigger_channel, bcr_channel, previous_event_time, previous_last_bcr):
-        bcr_count = 0
         tdcEvent = tdc5Reads #actual pile of data (timestamp, data)
         current_bcr = None
         counter = 0
@@ -196,8 +201,7 @@ class AtlasAnalyser():
                     delta += period
 
                 if not current_bcr or current_bcr.hitTime != tdcHitTime: 
-                    bcr_count += 1            
-                    current_bcr = BCR(tdcHitTime, previous_event_time, bcr_count, error = abs(delta - interval) > 50)
+                    current_bcr = BCR(tdcHitTime, previous_event_time, error = abs(delta - interval) > 50)
                     self.anubis_data.append(current_bcr)
            
             elif tdcChannel == trigger_channel:
@@ -275,7 +279,6 @@ class AtlasAnalyser():
                     continue
             else:
                 continue
-            print(tdc5Reads[0][0])
             initial_timestamp = datetime.datetime.timestamp(tdc5Reads[0][0])
         print(initial_timestamp)
         current_timestamp = initial_timestamp
@@ -341,7 +344,7 @@ class AtlasAnalyser():
     def pairBCRwithEvents(self):
         anubis_pointer = 0
         atlas_pointer = 0
-        time_window = 90e-6
+        time_window = 1#e-6
         self.matches = []
         with tqdm(total=len(self.atlas_data), desc=f"Matching", unit='Events') as pbar:        
             for atlas_pointer in range(len(self.atlas_data)):
@@ -357,14 +360,15 @@ class AtlasAnalyser():
                     if bcr.timeStamp < hit_time - 10*time_window: #if it is too far away do binary search
                         """
                         anubis_pointer = self.binary_search(anubis_pointer, hit_time, time_window)
-                        i = anubis_pointer
+                        i = anubis_pointer          
                         bcr = self.anubis_data[i-1]
-                        if abs(bcr.timeStamp - hit_time) < time_window:
+                        if abs(bcr.timeStamp - hit_time) < time_window and not bcr.error:
                             for trigger in bcr.triggers:
                                 self.matches[atlas_pointer][-1].append(trigger)
                     elif abs(bcr.timeStamp - hit_time) < time_window:
-                        for trigger in bcr.triggers:                            #if abs(trigger.bcId - self.atlas_data.iloc[atlas_pointer]["BCID"]) < 20:
-                            self.matches[atlas_pointer][-1].append(trigger)
+                        if not bcr.error:
+                            for trigger in bcr.triggers:                            #if abs(trigger.bcId - self.atlas_data.iloc[atlas_pointer]["BCID"]) < 20:
+                                self.matches[atlas_pointer][-1].append(trigger)
                         i += 1
                     elif bcr.timeStamp > hit_time + time_window:
                         break
@@ -384,7 +388,7 @@ def BCRHistogram(data, atlas = False, plot = True):
                 else:
                     hist.append(round(trigger.bcId))
         #strange number of problems ~ 8193
-    bins = [i for i in range(0,3565)]
+    bins = [i for i in range(0,3564)]
     counts, _ = np.histogram(hist, bins=bins, density=True)
     if plot:
         bins = bins[:-1]
